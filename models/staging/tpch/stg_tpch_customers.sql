bf8fe41a5afe1ca6ac9dcf8fdcfd81d447f077bb
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-06-10 22:32:37 +0000

    Reverted Error and added column

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -8,14 +8,15 @@ rename as (
 
     select
     
-        c_custkey as customer_keys,
+        c_custkey as customer_key,
         c_name as name,
         c_address as address, 
         c_nationkey as nation_key,
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment
+        c_comment as comment,
+        null as null_test2
 
     from source
 
