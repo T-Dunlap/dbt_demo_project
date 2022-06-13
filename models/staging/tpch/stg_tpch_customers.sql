1aea5e9e9bfdebd08147cd95d0b409ddbb233c55
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-06-13 18:13:33 +0000

    Added Null field

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -15,7 +15,8 @@ rename as (
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment
+        c_comment as comment,
+        null as null_field
 
     from source
 
