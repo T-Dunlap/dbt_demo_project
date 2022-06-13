b6019ec7eb5257ec2804b8d1ccee8884d0f3be6d
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-06-13 17:49:46 +0000

    Renamed column

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -8,15 +8,14 @@ rename as (
 
     select
     
-        c_custkey as customer_key,
+        c_custkey as customer_keys,
         c_name as name,
         c_address as address, 
         c_nationkey as nation_key,
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment, 
-        null as null_test1
+        c_comment as comment
 
     from source
 
