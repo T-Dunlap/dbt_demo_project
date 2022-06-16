9f28ea95078d01b38a585e25c6b989f640365183
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-06-16 22:15:29 +0000

    undoing changes

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -15,8 +15,7 @@ rename as (
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment,
-        null as placeholder
+        c_comment as comment
 
     from source
 
