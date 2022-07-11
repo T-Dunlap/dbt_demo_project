c2eea6ca249ed3567b2c6c1d3dd96d42aad19035
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-07-11 22:09:41 +0000

    removing clerk_name from text of demo_scenario_adding_clerk

--- analyses/demo_scenario_adding_clerk.sql
+++ analyses/demo_scenario_adding_clerk.sql
@@ -1,7 +1,7 @@
 /* 
 SCENARIO: The "Orders Dashboard" needs to have a filter for Clerk Name. 
 
-ACTION: We need to add the clerk_name field to the dataset that fuels the dashboard. 
+ACTION: We need to add the "clerk name" field to the dataset that fuels the dashboard. 
 
 HOW DO WE GET THERE? 
     1. Identify where to make the change (add column)
