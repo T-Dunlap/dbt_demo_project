f12219b13f3843e359aa9198386fce750a7e91f4
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-07-11 22:01:14 +0000

    Updated core.yml to remove clerk_name documentation

--- analyses/demo_scenario_adding_clerk.sql
+++ analyses/demo_scenario_adding_clerk.sql
@@ -4,11 +4,10 @@ SCENARIO: The "Orders Dashboard" needs to have a filter for Clerk Name.
 ACTION: We need to add the clerk_name field to the dataset that fuels the dashboard. 
 
 HOW DO WE GET THERE? 
-1. Identify where to make the change (add column)
-2. Implement the change in Dev
-3. Validate and Test
-4. Document 
-5. Deploy 
+    1. Identify where to make the change (add column)
+    2. Implement the change in Dev
+    3. Test & Document 
+    4. Merge & Deploy 
 */
 
 SELECT * 
