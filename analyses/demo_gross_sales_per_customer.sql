738ada902593ae8fa198d5f10024b039765944e5
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2023-01-19 00:53:27 +0000

    editing demo contents

--- analyses/demo_gross_sales_per_customer.sql
+++ analyses/demo_gross_sales_per_customer.sql
@@ -1,16 +1,17 @@
 /*
 
-SCENARIO ----------------------------------------
+----- SCENARIO -----------------------------------------
+
 I need to create a report that shows the top customers by spend amount. 
 
-Come into work in the morning... look at my slack or my teams or my skype for business... 
+----- STEPS TO REPRODUCE -------------------------------
 
-
-STEPS TO REPRODUCE -------------------------------
 1. Write my Select Statement
-2. Add tests & document
+2. Add tests & document 
 3. Build in Dev 
 4. Deploy to Prod
+
+
 */
 
 
