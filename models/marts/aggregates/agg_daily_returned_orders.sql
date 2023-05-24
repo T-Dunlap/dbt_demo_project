2fa1d13d2b02be28444f694faac83d1c717d1ff3
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2023-05-24 19:56:17 +0000

    adding python files

--- models/marts/aggregates/agg_daily_returned_orders.sql
+++ models/marts/aggregates/agg_daily_returned_orders.sql
@@ -6,10 +6,11 @@ with fct_order_items as (
 final as (
     select
         fct_order_items.order_date as ds
-        , cast(1.0*count(case when is_return then order_item_key else null end) as float) as y
+        , cast(1.0*count(case when return_flag = 'R' then order_item_key else null end) as float) as y
     from fct_order_items
     group by 1
     order by 1 desc
 )
 
 select * from final  
+order by y desc
