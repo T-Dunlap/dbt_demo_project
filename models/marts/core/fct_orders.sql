ed59b64f650fd3c28db29aacd0352eb2959b6244
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2023-06-14 19:57:17 +0000

    undo

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -38,14 +38,12 @@ final as (
         orders.status_code,
         orders.priority_code,
         orders.ship_priority,
-        orders.clerk_name,  --10/21/2022 KJ: Commenting out as it's not needed. 
-
+        --orders.clerk_name,  --10/21/2022 KJ: Commenting out as it's not needed. 
         1 as order_count,                
         order_item_summary.gross_item_sales_amount,
         order_item_summary.item_discount_amount,
         order_item_summary.item_tax_amount,
-        order_item_summary.net_item_sales_amount,
-        'test column' as test_column
+        order_item_summary.net_item_sales_amount
         
     from
         orders
