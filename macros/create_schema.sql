982ccb6e341862472e7823a87538b966f05ffd23
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2024-08-13 23:18:23 +0000

    changes incoming

--- macros/create_schema.sql
+++
@@ -1,5 +0,0 @@
-{% macro create_schema(relation) %}
-{%- call statement('create_schema') -%}
-    create schema if not exists {{ relation.without_identifier() }} with managed access
-  {% endcall %}
-{% endmacro %}
